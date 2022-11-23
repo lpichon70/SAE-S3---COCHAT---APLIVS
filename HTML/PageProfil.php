474b1028f94fad0ee37ba221f5cfb15340ecded4
YanisTetevuide <yanis.tetevuide@iut-dijon.u-bougogne.fr>
2022-11-23 10:11:22 +0100

    finition de l'affichage des informations du profil de club (VoirProfil.php, manque le sigle)

--- HTML/PageProfil.php
+++ HTML/PageProfil.php
@@ -3,7 +3,7 @@
     require_once("PHP/VoirMonClub/BDDManager.php");
     $db = new PDO('mysql:host=localhost;dbname=grp-254_s3_sae', 'grp-254', '0k6zqrrr');
     $manager = new BDDManager($db);
-    $club = $manager->get(1);
+    $club = $manager->get(3);
 ?>
 
 <!DOCTYPE html>
@@ -17,59 +17,77 @@
 
 <body>
 
-    <form>
+
 
     <div id="banniere">
 
-        <input id="logo" name="logo" type="file">
+        <input id="sigle" name="sigle" type="file">
 
         <div>
 
             <div id="infoPartie1">
 
-                <p>Infomations du club: </p>
+                <p>Infomations du club : </p>
                 <p>
-                    Nom du club: <?php echo $club->getNomClub(); ?>
+                    Nom du club : <?php echo $club->getNomClub(); ?>
                 </p>
                 <p>
-                    Téléphone: <?php echo $club->getTelFixeClub(); ?>                  
+                    Ville : <?php echo $club->getVille(); ?>
                 </p>
                 <p>
-                    Email:
+                    Rue : <?php echo $club->getRue(); ?>
                 </p>
                 <p>
-                    Siège social:
+                    Complement de rue : <?php echo $club->getComplementRue(); ?>
                 </p>
                 <p>
-                    Fédération: 
+                    Code postal : <?php echo $club->getCodePostal(); ?>
+                </p>
+                <p>
+                    Numéro d'agrément : <?php echo $club->getNumAgrement(); ?>
+                </p>
+                <p>
+                    Fédération : <?php echo $club->getFederation(); ?>
+                </p>
+                <p>
+                    Statut : <?php echo $club->getStatut(); ?>
+                </p>
+                <p>
+                    Siret : <?php echo $club->getSiret(); ?>
+                </p>  
+                <p>
+                    Email : <?php echo $club->getMailClub(); ?>
+                </p>
+                <p>
+                    Site internet : <?php echo $club->getSiteClub(); ?>
+                </p>
+                <p>
+                    Réseau social : <?php echo $club->getReseau(); ?>
+                </p>
+                <p>
+                    Téléphone fixe : <?php echo $club->getTelFixeClub(); ?>                  
+                </p>
+                <p>
+                    Téléphone portable : <?php echo $club->getTelPortableClub(); ?>
                 </p>
             </div>
             <div id="infoPartie2">
                 <p>
-                    Président: 
-                    <input id="PrenomPres" name="PrenomPres" type="text" placeholder="Prénom">
-                    <input id="NomPres" name="NomPres" type="text" placeholder="Nom">
+                    Président : <?php echo $club->getCivilitePres()." ".$club->getNomPres()." ".$club->getPrenomPres(); ?>
                 </p>
                 <p>
-                    Téléphone du président: 
-                    <input id="telPres" name="telPres" type="tel">
+                    Téléphone du président : <?php echo $club->getTelPres(); ?>
                 </p>
                 <p>
-                    Email du président:
-                    <input id="mailPres" name="mailPres" type="email">
+                    Email du président : <?php echo $club->getMailPres(); ?>
                 </p>
                 <p>
-                    Date création:
-                    <input id="dateCreation" name="dateCreation" type="date">
+                    Téléphone secrétariat : <?php echo $club->getTelSecret(); ?>
                 </p>
                 <p>
-                    Mail secrétariat:
-                    <input id="mailSecret" name="mailSecret" type="mail">
+                    Mail secrétariat : <?php echo $club->getMailSecret(); ?>
                 </p>
-                <p>
-                    Téléphone secrétariat:
-                    <input id="telSecret" name="telSecret" type="tel">
-                </p>
+                
 
 
             </div>
@@ -79,18 +97,17 @@
         </div>
 
         <div id="Description">
-            <p>Descrpition du club: </p>
+            <p>Descrpition du club : </p>
             <p><textarea id="description" name="description"></textarea></p>
         </div>
 
         <div id="ListeMembre">
-            <p>Liste es membres: </p>
+            <p>Liste des membres : </p>
             <p><textarea id="listeMembre" name="listeMembre"></textarea></p>
         </div>
 
     </div>
 
-</form>
 
 </body>
 
