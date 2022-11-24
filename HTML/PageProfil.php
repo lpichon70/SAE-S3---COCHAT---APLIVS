a04201c75dd34b1c0179eda46a3ab7bfe4687db1
YanisTetevuide <yanis.tetevuide@iut-dijon.u-bougogne.fr>
2022-11-24 09:59:59 +0100

    essai d'application de css su rla pageProfil.php

--- HTML/PageProfil.php
+++ HTML/PageProfil.php
@@ -10,9 +10,9 @@
 <html lang="fr">
 
 <head>
-	<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-	<link rel="stylesheet" href="CSS/PageProfil.css" type="text/css">
-	<title>Page de profil</title>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <link rel="stylesheet" href="CSS/PageProfil.css" type="text/css">
+    <title>Page de profil</title>
 </head>
 
 <body>
@@ -49,97 +49,112 @@
 
     <br>
 
-    <div id="banniere">
+    <div>
+        <?php echo '<img class="imageProfil" src="'.$club->getSigle().'" alt=sigle>' ; ?>
+        <p>
+            Nom du club :
+            <?php echo $club->getNomClub(); ?>
+        </p>
+    </div>
 
-        <div>
-
-            <div id="infoPartie1">
-
-                <?php echo "<img src=".$club->getSigle()." alt=sigle>" ; ?>
-                <p>Infomations du club : </p>
-                <p>
-                    Nom du club : <?php echo $club->getNomClub(); ?>
-                </p>
-                <p>
-                    Ville : <?php echo $club->getVille(); ?>
-                </p>
-                <p>
-                    Rue : <?php echo $club->getRue(); ?>
-                </p>
-                <p>
-                    Complement de rue : <?php echo $club->getComplementRue(); ?>
-                </p>
-                <p>
-                    Code postal : <?php echo $club->getCodePostal(); ?>
-                </p>
-                <p>
-                    Numéro d'agrément : <?php echo $club->getNumAgrement(); ?>
-                </p>
-                <p>
-                    Fédération : <?php echo $club->getFederation(); ?>
-                </p>
-                <p>
-                    Statut : <?php echo $club->getStatut(); ?>
-                </p>
-                <p>
-                    Siret : <?php echo $club->getSiret(); ?>
-                </p>  
-                <p>
-                    Email : <?php echo $club->getMailClub(); ?>
-                </p>
-                <p>
-                    Site internet : <?php echo $club->getSiteClub(); ?>
-                </p>
-                <p>
-                    Réseau social : <?php echo $club->getReseau(); ?>
-                </p>
-                <p>
-                    Téléphone fixe : <?php echo $club->getTelFixeClub(); ?>                  
-                </p>
-                <p>
-                    Téléphone portable : <?php echo $club->getTelPortableClub(); ?>
-                </p>
-            </div>
-            <div id="infoPartie2">
-                <p>
-                    Président : <?php echo $club->getCivilitePres()." ".$club->getNomPres()." ".$club->getPrenomPres(); ?>
-                </p>
-                <p>
-                    Téléphone du président : <?php echo $club->getTelPres(); ?>
-                </p>
-                <p>
-                    Email du président : <?php echo $club->getMailPres(); ?>
-                </p>
-                <p>
-                    Téléphone secrétariat : <?php echo $club->getTelSecret(); ?>
-                </p>
-                <p>
-                    Mail secrétariat : <?php echo $club->getMailSecret(); ?>
-                </p>
-                
+    <div id="infoPartie1">
 
 
-            </div>
+        <p>Infomations du club : </p>
+
+        <p>
+            Ville :
+            <?php echo $club->getVille(); ?>
+        </p>
+        <p>
+            Rue :
+            <?php echo $club->getRue(); ?>
+        </p>
+        <p>
+            Complement de rue :
+            <?php echo $club->getComplementRue(); ?>
+        </p>
+        <p>
+            Code postal :
+            <?php echo $club->getCodePostal(); ?>
+        </p>
+        <p>
+            Numéro d'agrément :
+            <?php echo $club->getNumAgrement(); ?>
+        </p>
+        <p>
+            Fédération :
+            <?php echo $club->getFederation(); ?>
+        </p>
+        <p>
+            Statut :
+            <?php echo $club->getStatut(); ?>
+        </p>
+        <p>
+            Siret :
+            <?php echo $club->getSiret(); ?>
+        </p>
+        <p>
+            Email :
+            <?php echo $club->getMailClub(); ?>
+        </p>
+        <p>
+            Site internet :
+            <?php echo $club->getSiteClub(); ?>
+        </p>
+        <p>
+            Réseau social :
+            <?php echo $club->getReseau(); ?>
+        </p>
+        <p>
+            Téléphone fixe :
+            <?php echo $club->getTelFixeClub(); ?>
+        </p>
+        <p>
+            Téléphone portable :
+            <?php echo $club->getTelPortableClub(); ?>
+        </p>
+    </div>
+    <div id="infoPartie2">
+        <p>
+            Président :
+            <?php echo $club->getCivilitePres()." ".$club->getNomPres()." ".$club->getPrenomPres(); ?>
+        </p>
+        <p>
+            Téléphone du président :
+            <?php echo $club->getTelPres(); ?>
+        </p>
+        <p>
+            Email du président :
+            <?php echo $club->getMailPres(); ?>
+        </p>
+        <p>
+            Téléphone secrétariat :
+            <?php echo $club->getTelSecret(); ?>
+        </p>
+        <p>
+            Mail secrétariat :
+            <?php echo $club->getMailSecret(); ?>
+        </p>
 
 
-
-        </div>
-
-        <div id="Description">
-            <p>Descrpition du club : </p>
-            <p><textarea id="description" name="description"></textarea></p>
-        </div>
-
-        <div id="ListeMembre">
-            <p>Liste des membres : </p>
-            <p><textarea id="listeMembre" name="listeMembre"></textarea></p>
-        </div>
 
     </div>
+
+    <div id="Description">
+        <p>Descrpition du club : </p>
+        <p><textarea id="description" name="description"></textarea></p>
+    </div>
+
+    <div id="ListeMembre">
+        <p>Liste des membres : </p>
+        <p><textarea id="listeMembre" name="listeMembre"></textarea></p>
+    </div>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
     <script src="Js/Animation.js"></script>
 
 
 </body>
-</html>
 
+</html>
