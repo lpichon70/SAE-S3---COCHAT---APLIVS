67505770af73e0f842c05a803048ec1f43dc1492
unknown <lucas.raveau@gmail.com>
2022-11-24 09:44:51 +0100

    Ajout de toute la partie bootstrap au formulaire de signalement dans le javascript, l'utilisateur ne verra que les cases pour lesquelles il a coché oui

---
+++ HTML/formulaire_renseignement.js
@@ -0,0 +1,87 @@
+function showDelegues(){
+    if (document.getElementById("DeleguesOui").checked)
+    {
+        document.getElementById('deleg').className = ""; 
+    }
+    else
+    {
+        document.getElementById('deleg').className = "d-none";
+    } 
+}
+
+function showArbitres(){
+    if (document.getElementById("ArbitresOui").checked)
+    {
+        document.getElementById('arb').className = ""; 
+    }
+    else
+    {
+        document.getElementById('arb').className = "d-none";
+    } 
+}
+
+function showTerrains(){
+    if (document.getElementById("TerrainsOui").checked)
+    {
+        document.getElementById('terr').className = ""; 
+    }
+    else
+    {
+        document.getElementById('terr').className = "d-none";
+    } 
+}
+
+function showReport(){
+    if (document.getElementById("ReportOui").checked)
+    {
+        document.getElementById('rep').className = ""; 
+    }
+    else
+    {
+        document.getElementById('rep').className = "d-none";
+    } 
+}
+
+function showIncident1(){
+    if (document.getElementById("IncidentOui1").checked)
+    {
+        document.getElementById('incident1').className = ""; 
+    }
+    else
+    {
+        document.getElementById('incident1').className = "d-none";
+    } 
+}
+
+function showIncident2(){
+    if (document.getElementById("IncidentOui2").checked)
+    {
+        document.getElementById('incident2').className = ""; 
+    }
+    else
+    {
+        document.getElementById('incident2').className = "d-none";
+    } 
+}
+
+function showContact1(){
+    if (document.getElementById("ContactOui1").checked)
+    {
+        document.getElementById('contact1').className = ""; 
+    }
+    else
+    {
+        document.getElementById('contact1').className = "d-none";
+    } 
+}
+
+function showContact2(){
+    if (document.getElementById("ContactOui2").checked)
+    {
+        document.getElementById('contact2').className = ""; 
+    }
+    else
+    {
+        document.getElementById('contact2').className = "d-none";
+    } 
+}
