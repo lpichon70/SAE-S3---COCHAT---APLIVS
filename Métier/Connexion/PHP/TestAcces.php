52d35618debc4a96e54b8b34f213697d8fccaaa8
Lucas RAVEAU <lucas.raveau@iut-dijon.u-bourgogne.fr>
2022-12-16 10:08:33 +0100

    Création de la page d'après connexion avec son css

--- Métier/Connexion/PHP/TestAcces.php
+++ Métier/Connexion/PHP/TestAcces.php
@@ -6,8 +6,48 @@
 
 
 
-<html>
-    <h1>Vous avez réussi à vous connecter en tant que <?=$statut?></h1>
+<!DOCTYPE html>
+<html lang="fr">
 
-    <a href="../../../index.php" >Retourner au menu</a>
-</html>
+  <head>
+    <meta charset="utf-8">
+    <title>APPLIVS</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="../../../Images/icon_ballon.png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/TestAccess.css">
+  </head>
+
+  <body>
+
+    <div class="row"><hr></div>
+
+    <div class="l1">
+      <img src="../../../Images/logo.gif" alt="logo DDCS">
+    </div>
+
+    <div class="container">
+      <h1 class="gras"><span>VOUS</span> <span>ETES
+        <br> 
+         CONNECTES</span> <span>EN TEMPS</span>
+        <br>
+        <span>QUE</span> <span><?=$statut?></span></h2>
+    </div>
+
+    <div id="LesImages">
+      <img id="logoDDCS" src="../../../Images/DDCS.jpg" width="25%" height="25%" class="logo" alt="logo DDCS" />
+    </div>
+
+    <div>
+      <p class="lien ombreText">
+          <a href="../../../index.php">Accueil</a>
+      </p>
+      <p class="lien ombreText <?=$afficheClub?>">
+          <a href="../../../Métier/Signalement/formulaire_renseignement.php"  >Signaler</a>
+      </p>
+      <p class="lien ombreText <?=$afficheRecherche?>">
+        <a href="#">Rechercher</a>
+</p>
+    
+    </div>
+    </html>
+</body>
