876627720bb51eb63ebc869e98f94bcd6a96f65a
lpichon95 <lucas.pichon@iut-dijon.u-bourgogne.fr>
2023-01-07 16:42:16 +0100

    Suppression des pages de redirections lors de la connexion (vous êtes connecté en tant que...). Lorsqu'un utilisateur se connecte il est directement redirigé sur la page d'accueil.

--- Métier/Connexion/PHP/Index.php
+++ Métier/Connexion/PHP/Index.php
@@ -1,33 +1,13 @@
 <?php
     session_start();
 
-    //Permet de rediriger l'utilisateur lors de sa conexion en fonction de son status
-    function redirige($session)
-    {
-        switch($session)
-        {
-            case 'POLICE':
-                header("Location:TestAcces.php");
-            break;
-            case 'DDCS':
-                header("Location:TestAcces.php");
-            break;
-            case 'ADMIN':
-                header("Location:TestAcces.php");
-            break;
-            case 'CLUB':
-                header("Location:TestAcces.php");
-            break;
-        }
-    }
-
     //Reidirige l'utilisateur sur la page de connexion s'il n'a pas de status ou sur une autre page grâce à la méthode au dessus
     if ($_SESSION['statut'] != '')
     {
-        redirige($_SESSION['statut']);
+        header("Location:../../../index.php");
     }
     else 
     {
-        header("Location:../../Connexion.php");
+        header("Location:../Connexion.php");
     }
 ?>
